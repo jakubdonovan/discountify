fe5add1042b97f5b4d0e244b338d98f92d048a11
jakub <jakubdonovan@protonmail.com>
2021-02-25 11:42:04 +0000

    added seo component to all pages

--- src/pages/terms.tsx
+++ src/pages/terms.tsx
@@ -1,9 +1,11 @@
 import React from "react"
+import SEO from "../components/seo"
 interface TermsProps {}
 
 const Terms: React.FC<TermsProps> = ({}) => {
   return (
     <div>
+      <SEO title="Terms" />
       <h1 className="text-2xl font-bold font-proxima">Terms and Conditions</h1>
     </div>
   )
