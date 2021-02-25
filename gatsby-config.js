6e9e1d68a7a157897797ba82079667f062e454d8
jakub <jakubdonovan@protonmail.com>
2021-02-25 00:36:34 +0000

    lazy loading images + converted to webp

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `discountify - 50 OFF uberEats`,
+    description: `With discountify, you can get 50% off all uber eats orders.`,
+    author: `discountify`,
   },
   plugins: [
     `gatsby-plugin-postcss`,
@@ -30,6 +30,6 @@ module.exports = {
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    `gatsby-plugin-offline`,
   ],
 }
---
+++ src/pages/terms.tsx
@@ -0,0 +1,8 @@
+import React from "react"
+interface TermsProps {}
+
+const Terms: React.FC<TermsProps> = ({}) => {
+  return <div></div>
+}
+
+export default Terms
